b7c4e84a223d1c2e721bc2f8c92301e82bf682f0
Dicko <macbook@Macbook-linux.local>
2024-05-12 13:23:26 +0700

    start

---
+++ FavFoods/FavFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavFoodsApp.swift
+//  FavFoods
+//
+//  Created by Dicko on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FavFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
